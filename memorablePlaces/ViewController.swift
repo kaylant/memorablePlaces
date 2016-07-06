3da2c0d51b497689a9f63a12c3b5a5647da15bfd
kaylant <kaylant@gmail.com>
2016-07-05 21:09:25 -0500

    initial commit, navigation and map set up

---
+++ memorablePlaces/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  memorablePlaces
+//
+//  Created by Kaylan Smith on 7/5/16.
+//  Copyright © 2016 Kaylan Smith. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
