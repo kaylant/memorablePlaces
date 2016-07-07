24460ac0877c71e9776710b49e42ff6b12b04e53
kaylant <kaylant@gmail.com>
2016-07-06 22:03:04 -0500

    added pins to map with long press

--- memorablePlaces/ViewController.swift
+++ memorablePlaces/ViewController.swift
@@ -9,13 +9,69 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var manager: CLLocationManager!
 
     @IBOutlet var map: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        var uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        
+        uilpgr.minimumPressDuration = 2.0
+        
+        map.addGestureRecognizer(uilpgr)
+        
+    }
+    
+    func action(gestureRecognizer: UILongPressGestureRecognizer) {
+    
+        // don't want to accidentally capture several long presses (duplicates)
+        // runs only once for one long press
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+        
+            var touchPoint = gestureRecognizer.locationInView(self.map)
+            
+            var newCoordinate = map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+            
+            var annotation = MKPointAnnotation()
+            
+            annotation.coordinate = newCoordinate
+            
+            annotation.title = "New Annotation"
+            
+            self.map.addAnnotation(annotation)
+            
+        
+        }
+        
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [CLLocation]) {
+    
+        var userLocation:CLLocation = locations[0]
+        var latitude = userLocation.coordinate.latitude
+        var longitude = userLocation.coordinate.longitude
+        var location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        var latDelta:CLLocationDegrees = 0.01
+        var lonDelta:CLLocationDegrees = 0.01
+        
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        self.map.setRegion(region, animated: true)
+
+        
     }
 
     override func didReceiveMemoryWarning() {
